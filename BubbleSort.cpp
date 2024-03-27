dfe86046a524022fa98806c1d6da69d041715c2f
PriyanshuKholiya <161438659+PriyanshuKholiya@users.noreply.github.com>
2024-03-27 14:03:37 +0530

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -6,44 +6,43 @@ int array[MAX_SIZE];
 int size = 0;
 
 void inputArray();
-void bubbleSort();
+void bubbleSort();// logic is if i>i+1 then swap
 void printArray();
 
 int main() {
-    printf("Enter the size of the array: ");
-    scanf("%d", &size);
+printf("Enter the size of the array: ");
+scanf("%d", &size);
 
-    if (size <= 0 || size > MAX_SIZE) {
-        printf("Invalid size! Size should be between 1 and %d.\n", MAX_SIZE);
-        return 1;
+if (size <= 0 || size > MAX_SIZE) {//size of an array cannot be zero or negative
+printf("Invalid size! Size should be between 1 and %d.\n", MAX_SIZE);
+return 1;// indicates an error has occured and stops further execution of the code
     }
 
-    printf("Enter %d elements of the array:\n", size);
-    inputArray();
+printf("Enter %d elements of the array:\n", size);
+inputArray();
 
-    printf("\nArray before sorting:\n");
-    printArray();
+printf("\nArray before sorting:\n");
+printArray();
 
-    bubbleSort();
+bubbleSort();
 
-    printf("\nArray after sorting:\n");
-    printArray();
+printf("\nArray after sorting:\n");
+printArray();
 
     return 0;
 }
 
 void inputArray() {
-    for (int i = 0; i < size; i++) {
-        scanf("%d", &array[i]);
+ for (int i = 0; i < size; i++) {
+ scanf("%d", &array[i]);
     }
 }
 
 void bubbleSort() {
-    int temp;
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = 0; j < size - i - 1; j++) {
+int temp;
+for (int i = 0; i < size - 1; i++) {//loop for passes
+     for (int j = 0; j < size - i - 1; j++) {//loop for comparisions at the end of this loop for every pass the largest element is sorted 
             if (array[j] > array[j + 1]) {
-                // Swap array[j] and array[j + 1]
                 temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
@@ -53,9 +52,8 @@ void bubbleSort() {
 }
 
 void printArray() {
-    for (int i = 0; i < size; i++) {
-        printf("%d ", array[i]);
+for (int i = 0; i < size; i++) {
+ printf("\n%d ", array[i]);
     }
-    printf("\n");
 }
 
